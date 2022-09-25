45e6b83b52b8bed8ba0b995aa1a7dfb4c6d065c5
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-09-25 12:40:51 +0300

    completed asignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    return( 
+    <button 
+    onBlur={()=> console.log('Hey! Eyes on me!')}
+    onFocus={()=> console.log('Good!')}
+    >Eyes on me</button>
+    );
+ }
+ export default EyesOnMe;
